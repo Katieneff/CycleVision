256129c6f34ac1499cfa574aa2a93134409efc43
Katie Neff <neffk9@gmail.com>
2016-11-29 19:51:57 -0800

    Update headlight_tracking.py

--- motion_tracking/headlight_tracking.py
+++ motion_tracking/headlight_tracking.py
@@ -9,16 +9,12 @@ from collections import deque
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required = True,
 	help = "Path to the image to be scanned")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-	help="max buffer size")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (0, 0, 255)
-greenUpper = (255, 255, 255)
-pts = deque(maxlen=args["buffer"])
+# define the lower and upper boundaries of the headlights
+# in the HSV color space
+maskLower = (0, 0, 255)
+maskUpper = (255, 255, 255)
 
 # load the video
 camera = cv2.VideoCapture(args["video"])
@@ -41,7 +37,7 @@ while True:
 	hsv = cv2.cvtColor(orig, cv2.COLOR_BGR2HSV)
 
 	# Mask headlights. Ranges found with https://github.com/jrosebr1/imutils/blob/master/bin/range-detector
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.inRange(hsv, maskLower, maskUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
 
@@ -53,8 +49,6 @@ while True:
 	contours = orig.copy()
 	cv2.drawContours(contours, cnts, -1, (0,255,0), 3)
 	cv2.imshow("Contours", contours)
-	
-	i = 0
 
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
@@ -68,7 +62,7 @@ while True:
 			cY = int(M["m01"] / M["m00"])
 
 			# Used to ignore the sky (place on the lefthand side of the frame
-			# because the sky is the same color as the headlights
+			# because the sky is the same color as the headlights)
 			if cX < 150:
 				continue
 				
