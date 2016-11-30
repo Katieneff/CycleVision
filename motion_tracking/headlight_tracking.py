38892e9414d7c937cc55a35cd77a201972f866af
Katie Neff <neffk9@gmail.com>
2016-11-29 18:03:46 -0800

    Update headlight_tracking.py

--- motion_tracking/headlight_tracking.py
+++ motion_tracking/headlight_tracking.py
@@ -20,7 +20,7 @@ greenLower = (0, 0, 255)
 greenUpper = (255, 255, 255)
 pts = deque(maxlen=args["buffer"])
 
-# load the image and convert it to grayscale
+# load the video
 camera = cv2.VideoCapture(args["video"])
 
 
@@ -33,21 +33,21 @@ while True:
 	if args.get("video") and not grabbed:
 		print "Can't grab video"
 		continue
- 
+		
+ 	# Resize for faster processing
 	orig = imutils.resize(orig, width=600)
 
+	# Convert to HSV color to find headlights
 	hsv = cv2.cvtColor(orig, cv2.COLOR_BGR2HSV)
 
-	#cv2.imshow("HSV", hsv)
-
-
+	# Mask headlights. Ranges found with https://github.com/jrosebr1/imutils/blob/master/bin/range-detector
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
 
 
 	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
+	# (x, y) center of the contours
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
 	contours = orig.copy()
@@ -59,48 +59,38 @@ while True:
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
 		for i in range(0, len(cnts)):
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-		#	c = max(cnts, key=cv2.contourArea)
+			# find the minimum enclosing circle in the contour
 			((x, y), radius) = cv2.minEnclosingCircle(cnts[i])
 			
+			# Find center coordinates
 			M = cv2.moments(cnts[i])
 			cX = int(M["m10"] / M["m00"])
 			cY = int(M["m01"] / M["m00"])
 
+			# Used to ignore the sky (place on the lefthand side of the frame
+			# because the sky is the same color as the headlights
 			if cX < 150:
 				continue
+				
+			# If the headlight is on lefthand side, print left,
+			# if right, print right
 			if cX > 220:
 				print "right"
 			else:
 				print "left"
+			
 			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			i+=1
+			
 			# only proceed if the radius meets a minimum size
 			if radius < 10:
-				# draw the circle and centroid on the frame,
-				# then update the list of tracked points
+				# draw the circle and centroid on the frame
 				cv2.circle(orig, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 				cv2.circle(orig, center, 5, (0, 0, 255), -1)
 
-
-		#pts.appendleft(center)
-			# loop over the set of tracked points
-		#for i in xrange(1, len(pts)):
-			# if either of the tracked points are None, ignore
-			# them
-		#	if pts[i - 1] is None or pts[i] is None:
-		#		continue
-
-			# otherwise, compute the thickness of the line and
-			# draw the connecting lines
-	#		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-	#		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-	#	 show the frame to our screen
+		#show the frame to our screen
 		cv2.imshow("Circles", orig)
- 	key = cv2.waitKey(1) & 0xFF
+ 	
+	key = cv2.waitKey(1) & 0xFF
  
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
