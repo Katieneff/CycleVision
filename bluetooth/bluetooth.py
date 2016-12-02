db674b543edf8fef1b20f2d06bee1695447ad900
neffk2 <neffk9@gmail.com>
2016-12-02 15:09:35 -0800

    Improved headlight tracking algorithm and integrated Bluetooth signals

--- bluetooth/bluetooth.py
+++ bluetooth/bluetooth.py
@@ -1,18 +1,28 @@
+"""
+A Bluetooth wrapper class that uses the serial library of python
+to communicate with the BlueSMIRF bluetooth module
+"""
+
 import serial
-ser = serial.Serial("/dev/ttyAMA0")
-ser.baudrate = 115200
 
+class Bluetooth(object):
+	
+	ser
+	
+	def __init__(self, devFile, baudrate):
+		self.ser = serial.Serial(devFile)
+		self.ser.baudrate = baudrate
+		
+		# Wait for pairing connection
+		while True:
+			read = self.ser.read(1)
+			if read == "C"
+			break
+		
+	
+	def write(command)
+		ser.write(command)
 
-ser.write("$$$")
-
-read = ser.read(4)
-print(read)
-if read[0:3] != "CMD":
-    print("Could not enter command mode")
-
-ser.write("SM,0")
-read = ser.read(4)
-if read[0:3] == "ERR":
-    print("Error entering slave mode")
-
-ser.close()
+	
+	def close()
+		ser.close()
--- motion_tracking/headlight_tracking.py
+++ motion_tracking/headlight_tracking.py
@@ -4,27 +4,47 @@ import glob
 import argparse 
 import imutils
 from collections import deque
+#from bluetooth import Bluetooth
+
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required = True,
 	help = "Path to the image to be scanned")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-	help="max buffer size")
 args = vars(ap.parse_args())
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (0, 0, 255)
-greenUpper = (255, 255, 255)
-pts = deque(maxlen=args["buffer"])
+"""
 
-# load the image and convert it to grayscale
+# Initialize and wait for pair
+bluetooth = Bluetooth("/dev/ttyAMA0/", 115000)
+
+"""
+
+# define the lower and upper boundaries of the headlights
+# in the HSV color space
+maskLower = (0, 0, 255)
+maskUpper = (255, 255, 255)
+
+# Range of locations for cars. 
+# backLeft is the blindspot area around 7 o'clock
+# backBackLeft is behind the blind spot area, further back
+# TODO: Make one for rear
+backLeftRangeX = (220, 320)
+backLeftRangeY = (0, 50)
+backBackLeftRangeX = (155, 219)
+backBackLeftRangeY = (75, 275)
+
+
+backLeft = False
+backBackLeft = False
+
+# load the video
 camera = cv2.VideoCapture(args["video"])
 
 
 while True:
+	backLeft = False
+	backbackLeft = False
 	# grab the current frame
 	(grabbed, orig) = camera.read()
  
@@ -33,74 +53,77 @@ while True:
 	if args.get("video") and not grabbed:
 		print "Can't grab video"
 		continue
- 
+		
+ 	# Resize for faster processing
 	orig = imutils.resize(orig, width=600)
 
+	# Convert to HSV color to find headlights
 	hsv = cv2.cvtColor(orig, cv2.COLOR_BGR2HSV)
 
-	#cv2.imshow("HSV", hsv)
-
-
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	# Mask headlights. Ranges found with https://github.com/jrosebr1/imutils/blob/master/bin/range-detector
+	mask = cv2.inRange(hsv, maskLower, maskUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
 
 
 	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
+	# (x, y) center of the contours
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
 	contours = orig.copy()
 	cv2.drawContours(contours, cnts, -1, (0,255,0), 3)
 	cv2.imshow("Contours", contours)
-	
-	i = 0
 
 	# only proceed if at least one contour was found
 	if len(cnts) > 0:
 		for i in range(0, len(cnts)):
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-		#	c = max(cnts, key=cv2.contourArea)
+			# find the minimum enclosing circle in the contour
 			((x, y), radius) = cv2.minEnclosingCircle(cnts[i])
 			
-			M = cv2.moments(cnts[i])
-			cX = int(M["m10"] / M["m00"])
-			cY = int(M["m01"] / M["m00"])
-
-			if cX < 150:
-				continue
-			if cX > 220:
-				print "right"
-			else:
-				print "left"
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			i+=1
-			# only proceed if the radius meets a minimum size
+			# Only proceed if radius is less than maximum size
 			if radius < 10:
-				# draw the circle and centroid on the frame,
-				# then update the list of tracked points
+
+				# Find center coordinates
+				M = cv2.moments(cnts[i])
+				cX = int(M["m10"] / M["m00"])
+				cY = int(M["m01"] / M["m00"])
+
+				# Used to ignore the sky (place on the lefthand side of the frame
+				# because the sky is the same color as the headlights)
+				if cX < 150:
+					continue
+					
+				# If the headlight is on lefthand side, print left,
+				# if right, print right
+				if cX > 220 and cX < 320:
+					backLeft = True
+				elif cX < 219 and cX > 155:
+					backBackLeft = True
+
+				
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+				
+				# draw the circle and centroid on the frame
 				cv2.circle(orig, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 				cv2.circle(orig, center, 5, (0, 0, 255), -1)
 
+		if backLeft:
+			print "back Left"
+			#bluetooth.write("1xx")
+		else:
+			print ""
+			#bluetooth.write("0xx")
+		if backBackLeft:
+			print "back back left"
+			#bluetooth.write("1xx")
+		else:
+			print ""
+			#bluetooth.write("0xx")
 
-		#pts.appendleft(center)
-			# loop over the set of tracked points
-		#for i in xrange(1, len(pts)):
-			# if either of the tracked points are None, ignore
-			# them
-		#	if pts[i - 1] is None or pts[i] is None:
-		#		continue
-
-			# otherwise, compute the thickness of the line and
-			# draw the connecting lines
-	#		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-	#		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-	#	 show the frame to our screen
-		cv2.imshow("Circles", orig)
- 	key = cv2.waitKey(1) & 0xFF
+		# show the frame to our screen
+		cv2.imshow("Headlights", orig)
+ 	
+	key = cv2.waitKey(1) & 0xFF
  
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
